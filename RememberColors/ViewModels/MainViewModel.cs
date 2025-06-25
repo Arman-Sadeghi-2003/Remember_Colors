6e7d1063010c126a3333b954e5f74a5809c2d49c
Arman Sadeghi <terminator_arman@yahoo.com>
2025-06-25 09:36:39 +0330

    minor vm change

--- RememberColors/ViewModels/MainViewModel.cs
+++ RememberColors/ViewModels/MainViewModel.cs
@@ -1,8 +1,51 @@
-﻿namespace RememberColors.ViewModels
+﻿using System.Windows;
+
+namespace RememberColors.ViewModels
 {
 	public class MainViewModel : ViewModelBase
 	{
+		#region Fields
 
+		private Visibility startButtonVisibility;
+
+		private bool mediumButtonEnabled;
+		private bool hardButtonEnabled;
+
+		#endregion
+
+		#region Properties
+
+		public Visibility StartButtonVisibility
+		{
+			get => startButtonVisibility;
+			set
+			{
+				startButtonVisibility = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public bool MediumButtonEnabled
+		{
+			get => mediumButtonEnabled;
+			set
+			{
+				mediumButtonEnabled = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public bool HardButtonEnabled
+		{
+			get => hardButtonEnabled;
+			set
+			{
+				hardButtonEnabled = value;
+				OnPropertyChanged();
+			}
+		}
+
+		#endregion
 
 	}
 }
