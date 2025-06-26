786323ee965d68d820d64eb8fbd5bc38d3d88860
Arman Sadeghi <Terminator_arman@yahoo.com>
2025-06-26 12:49:21 +0330

    minor

---
+++ RememberColors/Models/Settings/SettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace RememberColors.Models.Settings
+{
+	public class SettingsModel
+	{
+		#region Fields
+
+		private int showColorsNumber = 3;
+		private bool isRandom = true;
+		private List<Color> selectedColors = new();
+
+		#endregion
+	}
+}
